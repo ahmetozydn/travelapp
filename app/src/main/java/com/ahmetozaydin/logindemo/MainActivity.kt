0450da2c03de5607a450ea33df6a68a59eb641e6
ahmet <ahmetozaydn944@gmail.com>
2022-08-03 09:53:51 +0300

    The bottom navigation menu is added to project

--- app/src/main/java/com/ahmetozaydin/logindemo/MainActivity.kt
+++ app/src/main/java/com/ahmetozaydin/logindemo/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.ahmetozaydin.logindemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ahmetozaydin.logindemo.databinding.ActivityMainBinding
+
+ private var accountName : String? = null
+  private var password : String? = null
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -12,7 +18,29 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
+
+
+
+        binding.logInButton.setOnClickListener{
+            accountName = binding.editTextAccountName.text.toString()
+            password = binding.editTextPassword.text.toString()
+
+            if(accountName != null && password != null){//no box is empty// bu if else yapısını kuramadım buraya tekrar bakılacak
+                    //use Firebase to control the inputs
+            }else{//if one of the box is empty
+                Toast.makeText(applicationContext,"Kutucuklar boş bırakılamaz!",Toast.LENGTH_LONG).show()
+
+
+            }
+            val intent = Intent(this@MainActivity,MainScreenActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/ahmetozaydin/logindemo/MainScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.ahmetozaydin.logindemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
